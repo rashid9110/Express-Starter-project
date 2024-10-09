72571bdfb62da8ef7a0a3715756ae67871dde3c1
MD Rashid Alam <alamr9253@gmail.com>
2024-10-09 23:43:25 +0530

    Added serverConfig.js

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -2,6 +2,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 //Here we are exporting all the varibles that the project uses
+
 module.exports={
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL
