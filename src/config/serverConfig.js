a4c4368bbaf1715ef12350f81ed1a572afbe6261
MD Rashid Alam <alamr9253@gmail.com>
2024-10-09 18:43:28 +0530

    Connected to the mongodb server

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,8 @@
 const dotenv=require('dotenv');
 dotenv.config();
 
+//Here we are exporting all the varibles that the project uses
 module.exports={
     PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+
 const express=require("express")
 
 const ServerConfig= require('./config/serverConfig')
+const connectDB=require('./config/dbConfig')
 
 const app=express();
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async ()=>{
+    await connectDB();
     console.log(`Server started at port ${ServerConfig.PORT}`)
+
    
 })
